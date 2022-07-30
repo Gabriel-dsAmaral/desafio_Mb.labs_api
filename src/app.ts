5b57b8984067eda14bf9aab8854f9a0b447fe7ce
Gabriel <foioamaral16@gmail.com>
2022-07-30 20:33:25 -0300

    make it better
    Signed-off-by: Gabriel <foioamaral16@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -7,14 +7,9 @@ import { errorHandling } from "./middlewares";
 
 const app = express();
 
-app.use(express.json());
+app.use(cors());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*");
-  app.use(cors());
-  next();
-});
+app.use(express.json());
 
 registerRouters(app);
 
